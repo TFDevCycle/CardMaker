7f5148db047ede78d6b535cb6d796669f9124a56
TFDevCycle <tfdevcycle@gmail.com>
2022-05-02 16:13:56 +0200

    Fresh Start

--- modules/DrawImage.py
+++
@@ -1,8 +0,0 @@
-class DrawImage():
-
-   def __init__(self, card) -> None:
-
-      self.source_card = 'Card-' + card.lower().replace(' ', '-') + '.png'
-   
-   def getSourceCard(self):
-      return self.source_card
